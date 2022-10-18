6ef5f97694c55d5e8611d1c519aaae7b528c4d30
William <wildias@brasilseg.com.br>
2022-10-18 08:54:48 -0300

    feat: configurando arquivo de tema e fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold } from '@expo-google-fonts/archivo';
 
 import { Home } from "./src/screens/Home";
 
 export function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold
+  })
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <Home />
   )
