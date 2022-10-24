bcb165b6880bba8febc6bb024cec6f160f90693d
WilliamWJD <william.jose.wjd@gmail.com>
2022-10-24 08:32:13 -0300

    feat: finalizando componente acessory

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,7 +1,15 @@
 import { BackButton } from "../../components/BackButton";
 import { ImageSlider } from "../../components/ImageSlider";
+import { Acessory } from "../../components/Acessory";
 
-import { Container, Header, CarImages } from "./styles";
+import SpeedSvg from '../../assets/speed.svg';
+import AcelerationSvg from '../../assets/acceleration.svg';
+import ForceSvg from '../../assets/force.svg';
+import GasolineSvg from '../../assets/gasoline.svg';
+import ExchangeSvg from '../../assets/exchange.svg';
+import PeopleSvg from '../../assets/people.svg';
+
+import { Container, Header, CarImages, Content, Details, Description, Brand, Name, Rent, Period, Price, About, Acessories } from "./styles";
 
 export function CarDetails() {
     return (
@@ -14,6 +22,32 @@ export function CarDetails() {
                     imagesUrl={['https://www.pngmart.com/files/1/Audi-RS5-Red-PNG.png']}
                 />
             </CarImages>
+            <Content>
+                <Details>
+                    <Description>
+                        <Brand>Audi</Brand>
+                        <Name>Audo RS5</Name>
+                    </Description>
+
+                    <Rent>
+                        <Period>Ao dia</Period>
+                        <Price>R$ 580</Price>
+                    </Rent>
+                </Details>
+
+                <Acessories>
+                    <Acessory name="380 Km/h" icon={SpeedSvg} />
+                    <Acessory name="3.2s" icon={AcelerationSvg} />
+                    <Acessory name="800 HP" icon={ForceSvg} />
+                    <Acessory name="Gasolina" icon={GasolineSvg} />
+                    <Acessory name="Auto" icon={ExchangeSvg} />
+                    <Acessory name="4 Pessoas" icon={PeopleSvg} />
+                </Acessories>
+
+                <About>
+                    A personificação do mais puro dinamismo: uma parte frontal impressionante, que transborda potência, em conjunto com as linhas de fluxo exclusivas, uma postura ampla e dominante, assim como uma traseira impressionante. Impulsionado por um V6 Biturbo, com 450 CV.
+                </About>
+            </Content>
         </Container>
     )
 }
